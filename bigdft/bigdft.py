1747d768d0bc2d3739a447b4a51416f1d8714ada
ljbeal <louis.j.beal@gmail.com>
2023-04-25 08:42:33 +0200

    update annotations and docstrings

--- bigdft/bigdft.py
+++ bigdft/bigdft.py
@@ -50,7 +50,21 @@ class MiniLogger:
 @click.option('--submission', help='extra submission parameters')
 def run(structure=None,
         parameters=None,
-        submission=None):
+        submission=None) -> Logfile:
+    """
+    Run the calculation. Requires three file path inputs:
+
+    Args:
+        structure (str):
+            path to the serialised ASE json file
+        parameters (str):
+            path to the serialised BigDFTParameters yaml file
+        submission (str):
+            path to the serialised submission yaml file
+
+    Returns:
+        BigDFT.Logfile
+    """
     params_sub = {}
     if submission is not None:
         with open(submission, 'r') as o:
