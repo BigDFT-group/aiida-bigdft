1513a0f945df2f00c97175372fb852b0dadb0feb
ljbeal <louis.j.beal@gmail.com>
2023-11-02 11:56:12 +0100

    update debugging

--- aiida_bigdft/paths.py
+++ aiida_bigdft/paths.py
@@ -1,3 +1,4 @@
 DEBUG_PATHS = {'aiida': '/home/aiida/plugin_work/aiida.log',
                'test': '/home/test/pybigdft_plugin_volume/aiida.log',
-               'lb268778': '/W/lb268778/pybigdft_plugin_testing/aiida.log'}
+               'lb268778': '/W/lb268778/pybigdft_plugin_testing/aiida.log',
+               'ljbeal': '/home/ljbeal/work/aiida_debug/aiida.log'}
