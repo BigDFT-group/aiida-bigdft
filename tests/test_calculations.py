e2a119b7c688b592f9539586decf4461bd13d790
ljbeal <louis.j.beal@gmail.com>
2023-11-08 15:11:19 +0100

    calc test now properly tests bigdft

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,37 +1,33 @@
 """ Tests for calculations."""
-import os
-
 from aiida.engine import run
-from aiida.orm import SinglefileData
-from aiida.plugins import CalculationFactory, DataFactory
-
-from . import TEST_DIR
+from aiida.orm import StructureData
+from aiida.plugins import CalculationFactory
 
 
 def test_process(bigdft_code):
     """Test running a calculation
     note this does not test that the expected outputs are created of output parsing"""
 
-    # Prepare input parameters
-    DiffParameters = DataFactory("bigdft")
-    parameters = DiffParameters({"ignore-case": True})
-
-    file1 = SinglefileData(file=os.path.join(TEST_DIR, "input_files", "file1.txt"))
-    file2 = SinglefileData(file=os.path.join(TEST_DIR, "input_files", "file2.txt"))
-
     # set up calculation
+    cell = [[2, 0, 0], [0, 2, 0], [0, 0, 2]]
+    s = StructureData(cell=cell)
+    s.append_atom(position=(0, 0, 0), symbols="C")
+
     inputs = {
         "code": bigdft_code,
-        "parameters": parameters,
-        "file1": file1,
-        "file2": file2,
+        "structure": s,
         "metadata": {
-            "options": {"max_wallclock_seconds": 30},
+            "options": {
+                "jobname": "Mono_Carbon",
+                "max_wallclock_seconds": 60,
+                "resources": {
+                    "num_machines": 1,
+                },
+                "withmpi": False,
+            }
         },
     }
 
     result = run(CalculationFactory("bigdft"), **inputs)
-    computed_diff = result["bigdft"].get_content()
 
-    assert "content1" in computed_diff
-    assert "content2" in computed_diff
+    assert int(result["energy"].value) == -5
