792c24e620f100e48aae893b014a1370a8d20f67
ljbeal <louis.j.beal@gmail.com>
2024-01-08 15:49:20 +0100

    [clean] update formatting

--- aiida_bigdft/workflows/base.py
+++ aiida_bigdft/workflows/base.py
@@ -19,14 +19,15 @@ class BigDFTBaseWorkChain(BaseRestartWorkChain):
 
         spec.outline(
             cls.setup,
-            while_(cls.should_run_process)
-                (
-                cls.run_process,
-                cls.inspect_process
-                ),
+            while_(cls.should_run_process)(
+                    cls.run_process,
+                    cls.inspect_process
+                    ),
             cls.results,
         )
-        spec.exit_code(300, "ERROR_UNRECOVERABLE_FAILURE", message="The calculation encountered an unrecoverable error")
+        spec.exit_code(300,
+                       "ERROR_UNRECOVERABLE_FAILURE",
+                       message="The calculation encountered an unrecoverable error")
 
     def setup(self):
         """Call the `setup` of the `BaseRestartWorkChain` and then create the inputs dictionary in `self.ctx.inputs`.
@@ -36,7 +37,8 @@ class BigDFTBaseWorkChain(BaseRestartWorkChain):
         """
         super().setup()
         self.ctx.restart_calc = None
-        self.ctx.inputs = AttributeDict(self.exposed_inputs(BigDFTCalculation, BigDFTBaseWorkChain._workchain_namespace))
+        self.ctx.inputs = AttributeDict(self.exposed_inputs(BigDFTCalculation,
+                                                            BigDFTBaseWorkChain._workchain_namespace))
 
     def report_error_handled(self, calculation, action):
         """Report an action taken for a calculation that has failed.
@@ -46,8 +48,9 @@ class BigDFTBaseWorkChain(BaseRestartWorkChain):
         :param calculation: the failed calculation node
         :param action: a string message with the action taken
         """
-        arguments = [calculation.process_label, calculation.pk, calculation.exit_status, calculation.exit_message]
-        self.report('{}<{}> failed with exit status {}: {}'.format(*arguments))
+        self.report(f'{calculation.process_label}<{calculation.pk}> '
+                    f'failed with exit status {calculation.exit_status}: '
+                    f'{calculation.exit_message}')
         self.report(f'Action taken: {action}')
 
     @process_handler(priority=600)
